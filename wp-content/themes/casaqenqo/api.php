cbddd5f8947d20f49e41d499e0bee74b3bceb001
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-31 18:51:29 -0500

    upload some images

--- wp-content/themes/casaqenqo/api.php
+++ wp-content/themes/casaqenqo/api.php
@@ -5,13 +5,13 @@ add_action( 'rest_api_init', 'create_api_posts_meta_field' );
 function create_api_posts_meta_field() {
 
     // register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
-    register_rest_field( 'post', 'post-meta-fields', array(
+    register_rest_field( 'post', 'post_meta_fields', array(
            'get_callback'    => 'get_post_meta_for_api',
            'schema'          => null,
         )
     );
 
-    register_rest_field( 'page', 'post-meta-fields', array(
+    register_rest_field( 'page', 'post_meta_fields', array(
            'get_callback'    => 'get_post_meta_for_api',
            'schema'          => null,
         )
@@ -31,13 +31,13 @@ add_action( 'rest_api_init', 'create_api_posts_featured_images' );
 function create_api_posts_featured_images() {
 
     // register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
-    register_rest_field( 'post', 'featured-image', array(
+    register_rest_field( 'post', 'featured_image', array(
            'get_callback'    => 'get_post_featured_image_for_api',
            'schema'          => null,
         )
     );
 
-    register_rest_field( 'page', 'featured-image', array(
+    register_rest_field( 'page', 'featured_image', array(
            'get_callback'    => 'get_post_featured_image_for_api',
            'schema'          => null,
         )
